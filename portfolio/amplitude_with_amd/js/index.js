2785dbf4ddef9587b3916bce9797d754dca11667
cdll <cd234350150@gmail.com>
2015-11-23 20:51:19 +0800

    update jspm;

--- portfolio/amplitude_with_amd/js/index.js
+++ portfolio/amplitude_with_amd/js/index.js
@@ -1,28 +1,16 @@
-(function(t,e){var n=t.amplitude||{};var r=e.createElement("script");r.type="text/javascript";
-r.async=true;r.src="https://d24n15hnbwhuhn.cloudfront.net/libs/amplitude-2.6.1-min.gz.js";
-r.onload=function(){t.amplitude.runQueuedFunctions()};var s=e.getElementsByTagName("script")[0];
-s.parentNode.insertBefore(r,s);var i=function(){this._q=[];return this};function o(t){
-i.prototype[t]=function(){this._q.push([t].concat(Array.prototype.slice.call(arguments,0)));
-return this}}var a=["add","set","setOnce","unset"];for(var u=0;u<a.length;u++){o(a[u]);
-}n.Identify=i;n._q=[];function c(t){n[t]=function(){n._q.push([t].concat(Array.prototype.slice.call(arguments,0)));
-}}var l=["init","logEvent","logRevenue","setUserId","setUserProperties","setOptOut","setVersionName","setDomain","setDeviceId","setGlobalUserProperties","identify"];
-for(var p=0;p<l.length;p++){c(l[p])}t.amplitude=n})(window,document);
-
-//amplitude.init("YOUR_API_KEY_HERE");
-
 avalon.config({
-  debug: 0,
+//  debug: 0,
   paths: {
-    
+    amplitude: 'https://d24n15hnbwhuhn.cloudfront.net/libs/amplitude-2.6.1-min.gz.js'
   },
   shim: {
-    
   }
 })
 
 require(['avalon'], function($){
   console.info($.version)
 })
-//require(['amplitude'], function(){
-//  console.info(amplitude)
-//})
+require(['amplitude'], function(amplitude){
+  console.info(amplitude)
+//  amplitude.init("YOUR_API_KEY_HERE");
+})
