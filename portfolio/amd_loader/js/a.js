fad35317adf724c5179830a3604612f03ff8e5be
cdll <cd234350150@gmail.com>
2015-12-12 22:21:50 +0800

    add and loader demo to test jquery and ajaxmini;

---
+++ portfolio/amd_loader/js/a.js
@@ -0,0 +1,30 @@
+//require(['./js/b.js'], function(){
+//  document.write(query)
+//})
+require.config({
+  baseUrl: '/',
+  paths:{
+    jquery: '/bower_components/jquery/dist/jquery.min',
+    ajax: "/bower_components/ajax/dist/ajax.min"
+  },
+  shim:{
+    jquery:{
+      exports: 'jquery'
+    },
+    ajax:{
+      exports: 'Ajax'
+    }
+  },
+  timeout: 3
+})
+require(['jquery'], function($){
+  //todo 获取index.json中的json数据
+  $.getJSON('./index.json', function(res){
+    console.info(res)
+  })
+})
+
+//require(['ajax'], function(){
+//  var ajax= new Ajax()
+//  console.info(ajax)
+//})
---
+++ portfolio/amd_loader/js/b.js
@@ -0,0 +1,6 @@
+if(location.search){
+  query= location.search
+}
+else{
+  query= '页面参数不存在'
+}
