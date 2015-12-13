53e18d9a50a477f79ed37f566b425c1cf4678e1c
cdll <cd234350150@gmail.com>
2015-12-13 21:31:26 +0800

    suck ajax;

--- portfolio/amd_loader/js/a.js
+++ portfolio/amd_loader/js/a.js
@@ -15,16 +15,16 @@ require.config({
       exports: 'Ajax'
     }
   },
-  timeout: 3
+  timeout: 5
 })
-require(['jquery'], function($){
-  //todo 获取index.json中的json数据
-  $.getJSON('./index.json', function(res){
-    console.info(res)
-  })
-})
-
-//require(['ajax'], function(){
-//  var ajax= new Ajax()
-//  console.info(ajax)
+//require(['jquery'], function($){
+//  //todo 获取index.json中的json数据
+//  $.getJSON('./index.json', function(res){
+//    console.info(res)
+//  })
 //})
+
+require(['ajax'], function(){
+  var ajax= new Ajax()
+  console.info(ajax)
+})
