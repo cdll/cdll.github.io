68e71cb77305491d64b51f956411b46ca172addc
cdll <cd234350150@gmail.com>
2016-01-25 23:15:13 +0800

    update

--- portfolio/gotu/gotu.js
+++ portfolio/gotu/gotu.js
@@ -10,32 +10,13 @@ var gotu= avalon.define({
   $id: 'gotu'
   ,input: ''
   ,test: 0
-  ,list: [
-    'AA'
-    ,'BB'
-    ,'CC'
-    ,'DD'
-    ,'EE'
-    ,'FF'
-    ,'EE'
-    ,'GG'
-    ,'HH'
-    ,'II'
-    ,'JJ'
-    ,'KK'
-    ,'LL'
-    ,'MM'
-    ,'NN'
-    ,'OO'
-    ,'PP'
-    ,'QQ'
-  ]
+  ,list: localStorage.gotu? localStorage.gotu.split(','): []
   ,selected: []
   ,running: 0
   ,compute: function(){
     if(!gotu.list.length) return false
     function ran(){
-      gotu.test= Math.floor(Math.random()* (gotu.list.length||25))
+      gotu.test= Math.floor(Math.random()* gotu.list.length)
     }
     if(gotu.running== 1){
       gotu.running= 0
@@ -49,6 +30,12 @@ var gotu= avalon.define({
   }
   ,addList: function(e){
     if(e.which== 13&& gotu.input.replace(/\s/g)){
+      if(localStorage.gotu){
+        localStorage.gotu+= ','+ gotu.input
+      }
+      else{
+        localStorage.gotu= [gotu.input]
+      }
       gotu.list.push(gotu.input)
       gotu.input= ''
     }
