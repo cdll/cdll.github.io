44144c510b46e64c3b947265cfdfb177bfb72281
CDLL <cd234350150@gmail.com>
2016-03-26 22:22:40 +0800

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 require(['/loaderConfig'], function(config){
   avalon.config(config)
-  
+
   require(['mock'], function(Mock){
     var data= Mock.mock({
       'list|1-10': [
@@ -11,8 +11,8 @@ require(['/loaderConfig'], function(config){
     })
     console.log(JSON.stringify(data))
   })
-  
-  require(['qwest', 'bs', 'css!bower_components/bootstrap/bootstrap-3.3.6/dist/css/bootstrap.min'], function(ajax){
+
+  require(['qwest', 'mdl'], function(ajax){
     console.log(ajax, $)
   })
 })
