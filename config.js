ab6471c6dacb6c4ce076ae07aa861ea527896458
cdll <cd234350150@gmail.com>
2016-06-19 22:23:02 +0800

    jspm test;

---
+++ config.js
@@ -0,0 +1,14 @@
+
+System.config({
+  baseUrl: './'
+  ,map:{
+  }
+  ,paths: {
+    vue: '/bower_components/vue/dist/vue.min.js'
+    ,qwest: '/bower_components/qwest/qwest.min.js'
+    ,mdl: '/bower_components/material-design-lite/material.min.js'
+  }
+  ,meta: {
+    Mock: '/bower_components/mockjs/dist/mock-min.js'
+  }
+})
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
+'use strict'
 
-let Mock= require('/bower_components/mockjs/dist/mock-min.js')
-console.info(Mock)
+//let Mock= require('/bower_components/mockjs/dist/mock-min.js')
+//console.info(Mock)
 //var data= Mock.mock({
 //  'list|1-10': [
 //    {
@@ -10,30 +11,31 @@ console.info(Mock)
 //})
 //console.log(JSON.stringify(data))
 
-let mdl= require('/bower_components/material-design-lite/material.min.js')
-let ajax= require('/bower_components/qwest/qwest.min.js')
+let mdl= require('mdl')
+let ajax= require('qwest')
+let qwest= require('node_modules/qwest/qwest.min.js')
 console.info(ajax.xhr2, mdl)
-ajax.get(
-  'https://api.github.com/users/cdll/repos'
-  ,null
-  ,{
-    headers: {
-      Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
-      ,'Upgrade-Insecure-Requests': 1
-      ,nonce: (new Date()).getTime()
-    }
-    ,dataType: 'application/jsonp'
-  }
-)
-.then(function(xhr, res){
-  console.info(xhr, res)
-})
-.catch(function(xhr, res, err){
-  console.warn(xhr, res, err)
-})
+//ajax.get(
+//  'https://api.github.com/users/cdll/repos'
+//  ,null
+//  ,{
+//    headers: {
+//      Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
+//      ,'Upgrade-Insecure-Requests': 1
+//      ,nonce: (new Date()).getTime()
+//    }
+//    ,dataType: 'application/jsonp'
+//  }
+//)
+//.then(function(xhr, res){
+//  console.info(xhr, res)
+//})
+//.catch(function(xhr, res, err){
+//  console.warn(xhr, res, err)
+//})
 
 setTimeout(()=>{
-  let Vue= require('/bower_components/vue/dist/vue.min.js')
+  let Vue= require('vue')
 
   console.info('Vue:', Vue.version)
 }, 1000)
