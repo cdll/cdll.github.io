bfafcff2c579d3b714bfafc50ba2a4d2b829a2c2
cdll <cd234350150@gmail.com>
2017-01-08 13:32:00 +0800

    optimuzed riot seo test;

--- portfolio/riot-todo/seo.js
+++ portfolio/riot-todo/seo.js
@@ -3,13 +3,21 @@
 
 var riot= require('riot')
 
-// var fs = require('fs')
-// require.extensions['.html'] = function (module, filename) {
+var fs = require('fs')
+// require.extensions['.tag.html'] = function (module, filename) {
 //   module.exports = fs.readFileSync(filename, 'utf8')
 // }
+// var tag= requireTagHTML('./tags/my-tag.tag.html')
 
-var tag= require('./modules/my-tag.tag')
-// var tag= riot.compile('./modules/my-tag.tag')
+// var requireTagHTML= (filePath)=>{
+//   return fs.readFileSync(filePath, {
+//     encoding: 'utf8'
+//   })
+// }
+// var tag= requireTagHTML('./tags/my-tag.tag.html')
+
+var tag= require('./tags/my-tag.tag')
+// var tag= riot.compile('./tags/my-tag.tag')
 var res= riot.render(tag, {
   name: 'cdll'
 })
