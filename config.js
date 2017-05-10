fc04613eb67cc1da27018722fab0a54a1f560b3f
cdll <cd234350150@gmail.com>
2017-05-10 22:23:36 +0800

    update darlingshow by system.js;

--- config.js
+++ config.js
@@ -1,21 +1,22 @@
 
 System.config({
-  "baseUrl": "./",
-  "bundles": {
+  baseUrl: "./"
+  ,bundles: {
     "vueApp.min.js": [
       "admin/src/vueApp.js"
     ]
-  },
-  map: {
-  },
-  meta: {
+  }
+  ,map: {
+  }
+  ,meta: {
     Mock: "/bower_components/mockjs/dist/mock-min.js"
-  },
-  paths: {
+  }
+  ,paths: {
     mdl: "/bower_components/material-design-lite/material.min.js"
     ,qwest: "/bower_components/qwest/qwest.min.js"
     ,vue: "/bower_components/vue/dist/vue.min.js"
     ,axios: "/bower_components/axios/dist/axios.min.js"
     ,riot: "/bower_components/riot/riot+compiler.min.js"
+    ,swiper: '/bower_components/Swiper/dist/js/swiper.min.js'
   }
 })
