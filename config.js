a1d4ca529191104e687fb9af224bb7d090300c6b
cdll <cd234350150@gmail.com>
2019-03-03 18:45:49 +0800

    replace cdn.statifile.org by cdn.bootcss.com;

--- config.js
+++ config.js
@@ -13,14 +13,14 @@ System.config({
   ,meta: {
   }
   ,paths: {
-    mdl: "https://cdn.staticfile.org/material-design-lite/1.3.0/material.min.js"
-    ,Mock: "https://cdn.staticfile.org/Mock.js/1.0.1-beta3/mock-min.js"
-    // ,qwest: "https://cdn.staticfile.org/qwest/4.4.6/qwest.min.js"
-    ,vue: "https://cdn.staticfile.org/vue/1.0.28/vue.min.js"
-    ,axios: "https://cdn.staticfile.org/axios/0.19.0-beta.1/axios.min.js"
+    mdl: "https://cdn.bootcss.com/material-design-lite/1.3.0/material.min.js"
+    ,Mock: "https://cdn.bootcss.com/Mock.js/1.0.1-beta3/mock-min.js"
+    // ,qwest: "https://cdn.bootcss.com/qwest/4.4.6/qwest.min.js"
+    ,vue: "https://cdn.bootcss.com/vue/1.0.28/vue.min.js"
+    ,axios: "https://cdn.bootcss.com/axios/0.19.0-beta.1/axios.min.js"
     ,riot: "https://system-unsafe-production.jspm.io/riot@3"
     ,riotc: "https://system-unsafe-production.jspm.io/riot@3/riot+compiler"
-    ,swiper: 'https://cdn.staticfile.org/Swiper/3.4.2/js/swiper.min.js'
+    ,swiper: 'https://cdn.bootcss.com/Swiper/3.4.2/js/swiper.min.js'
     ,md5: 'npm:js-md5'
   }
 })
--- src/js/require.config.js
+++ src/js/require.config.js
@@ -3,15 +3,15 @@ var _paths= (function(){
   var libpaths= {
     avalon: {
       dev: '/bower_components/avalon/dist/avalon.shim.min'
-      ,prd: '//cdn.staticfile.org/avalon.js/1.5.9/avalon.shim.min'
+      ,prd: '//cdn.bootcss.com/avalon.js/1.5.9/avalon.shim.min'
     }
     ,mmRouter: {
       dev: "/bower_components/mmRouter/mmRouter"
-      ,prd: "//cdn.staticfile.org/mmRouter/0.3/mmRouter"
+      ,prd: "//cdn.bootcss.com/mmRouter/0.3/mmRouter"
     }
     ,mmHistory: {
       dev: "/bower_components/mmRouter/mmHistory"
-      ,prd: '//cdn.staticfile.org/mmRouter/0.3/mmHistory'
+      ,prd: '//cdn.bootcss.com/mmRouter/0.3/mmHistory'
     }
     ,mmState: {
       dev: "/bower_components/mmRouter/mmState"
@@ -23,30 +23,30 @@ var _paths= (function(){
     }
     ,qwest: {
       dev: "/bower_components/qwest/qwest.min"
-      ,prd: '//cdn.staticfile.org/qwest/4.4.5/qwest.min'
+      ,prd: '//cdn.bootcss.com/qwest/4.4.5/qwest.min'
     }
     ,mock: {
       dev: "/bower_components/mockjs/dist/mock-min"
       ,prd: "/bower_components/mockjs/dist/mock-min"
     }
     ,bsn: {
-      dev: '//cdn.staticfile.org/bootstrap.native/1.1.0/bootstrap-native.min'
+      dev: '//cdn.bootcss.com/bootstrap.native/1.1.0/bootstrap-native.min'
     }
     ,bs: {
       dev: "/bower_components/bootstrap/bootstrap-3.3.6/dist/js/bootst  ,prdrap.min"
-      ,prd: '//cdn.staticfile.org/twitter-bootstrap/3.3.7/js/bootstrap.min'
+      ,prd: '//cdn.bootcss.com/twitter-bootstrap/3.3.7/js/bootstrap.min'
     }
     ,jquery: {
       dev: '/bower_components/jquery/dist/jquery.min'
-      ,prd: '//cdn.staticfile.org/jquery/1.12.4/jquery.min'
+      ,prd: '//cdn.bootcss.com/jquery/1.12.4/jquery.min'
     }
     ,mdl: {
       dev: "/bower_components/material-design-lite/material.min"
-      ,prd: '//cdn.staticfile.org/material-design-lite/1.3.0/material.min'
+      ,prd: '//cdn.bootcss.com/material-design-lite/1.3.0/material.min'
     }
     ,vue: {
       dev: '/bower_components/vue/dist/vue.min'
-      ,prd: '//cdn.staticfile.org/vue/2.2.1/vue.min'
+      ,prd: '//cdn.bootcss.com/vue/2.2.1/vue.min'
     }
   }
   function getPath(key){
